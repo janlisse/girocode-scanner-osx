df3df20ff0d3e1c0b5467e1c6a405f62f6522b1f
Jan Lisse <jan.lisse@gmail.com>
2017-07-27 13:25:42 +0200

    Initial Commit

---
+++ girocode-scanner-osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  girocode-scanner-osx
+//
+//  Created by Jan Liße on 27.07.17.
+//  Copyright © 2017 immaterial industries. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ girocode-scanner-osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  girocode-scanner-osx
+//
+//  Created by Jan Liße on 27.07.17.
+//  Copyright © 2017 immaterial industries. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
